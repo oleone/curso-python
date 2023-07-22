07e49c5bec080933ee5f93993f08c546b7748c8b
Bruno Leone <leonecbruno@gmail.com>
2023-07-22 20:47:21 -0300

    Aula 1

---
+++ aula-1/script.py
@@ -0,0 +1,18 @@
+# Aqui são comentários no python
+print("Esse código é lido e interpretado")
+
+print("Aqui sim") # Comentario
+
+"""
+DocString pode ser usado como multi comentários
+
+loren
+
+"""
+
+'''
+DocString também usado como multi comentários
+
+loren
+
+'''
